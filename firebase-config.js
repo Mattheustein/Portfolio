5cf452fc9bb1662ec4f953f3c0caff2ac8910f4b
Mahmoud Sultan <swe.mahmoud.sultan@gmail.com>
2026-05-22 04:58:12 +0300

    Update Firebase app ID and measurement ID to new web app registration

--- firebase-config.js
+++ firebase-config.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   projectId: "mattheustein-03",
   storageBucket: "mattheustein-03.firebasestorage.app",
   messagingSenderId: "1027709330385",
-  appId: "1:1027709330385:web:cbb255f6bee6ef3419b6f0",
-  measurementId: "G-QH9R8Z80CT"
+  appId: "1:1027709330385:web:4ee08eeb3984cff819b6f0",
+  measurementId: "G-9RTHGXFD5V"
 };
 firebase.initializeApp(firebaseConfig);
